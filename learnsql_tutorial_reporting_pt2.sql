caa6655ddee7c6f8cbc1e03424c7fd44e39c85f9
Scott Davis <scdavis6@gmail.com>
2025-06-18 22:43:32 -0700

    Update learnsql_tutorial_reporting_pt2.sql

--- learnsql_tutorial_reporting_pt2.sql
+++ learnsql_tutorial_reporting_pt2.sql
@@ -2,11 +2,10 @@
 -- Data: Tableau Superstore - Orders
 -- Language: MySQL on MySQL Workbench
 
-
 /*
 Content:
 Analyzing Customers
-	-Finance Metrics
+	-Customers
     -Orders
     -Segmentation
 
@@ -14,7 +13,6 @@ Analyzing Products
 	-Product Reports
     -Case Question
 */
-
 
 /*
 Analyzing Customers
@@ -37,8 +35,39 @@ How many customers are there and by time period?
 
 
 /*
-Create a report of customer-based finance metrics
-TO WRITE
+Create a report of customer-based finance metrics per product category and sub category:
+-Average Revenue Per User (ARPU)
+-Average Profit Per User (APPU)
 */
 
+-- SELECT
+-- 	  Category,
+--       Sub_Category,
+--       ROUND(SUM(Sales) / COUNT(DISTINCT `Customer_ID`), 1) AS ARPU,
+--       ROUND(SUM(Profit) / COUNT(DISTINCT `Customer_ID`), 1) AS APPU,
+-- FROM 
+--     `Tableau Superstore`.orders
+-- GROUP BY
+-- 	1, 2
+-- ORDER BY 
+-- 	1, 2
 
+/*
+Orders
+*/
+
+/*
+Create a report of order-based finance metrics
+-- Average Quantity Per User (AQPU)
+-- Average Order Value (AOV)
+*/
+
+-- SELECT
+-- 	ROUND(SUM(Quantity) / COUNT(DISTINCT `Customer_ID`), 1) AS AQPU,
+-- 	ROUND(SUM(Sales)/COUNT(order_id), 2) as average_order_value
+-- FROM 
+--     `Tableau Superstore`.orders;
+
+/*
+Medium
+*/
